61ba848147b282f1394581d897b220f611c63f42
j-o-phillips <jphillips105@googlemail.com>
2024-04-19 15:50:40 +0200

    tables

--- app/gpu/page.tsx
+++
@@ -1,9 +0,0 @@
-const GpuCompare = () => {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      Page under construction
-    </div>
-  );
-};
-
-export default GpuCompare;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/navbar/Navbar";
 
+import { NextUIProvider } from "@nextui-org/react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <NextUIProvider>
+          <Navbar />
+          {children}
+        </NextUIProvider>
       </body>
     </html>
   );
